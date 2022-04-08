66ad81752e2f7ba6387ca965a664001872078f3c
Justin Li <justinlit23@gmail.com>
2022-04-08 00:52:35 -0700

    commit tree b57120bb24ac36112ef0667c3ee9da9031c50e99

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+const superbowlWin = (arr) => {
+    const winningYearObject = arr.find(
+        (object) => object.result === 'W'
+    )
+    if (winningYearObject) {
+    return winningYearObject.year
+    }
+}
+
+/*const sampleArray =  [
+    {year: "2015", result: "W"},
+    {year: "2016", result: "W"}
+]*/
